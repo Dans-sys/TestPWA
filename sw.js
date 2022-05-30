5ab5e35f6b0d54c5ebd756b97a61fcc508fb0bd6
Daniel Solano <daniel.solano@azucareralagrecia.com>
2022-05-30 15:11:42 -0600

    Versionamiento SW

--- sw.js
+++ sw.js
@@ -1,8 +1,8 @@
 // imports
 importScripts('js/sw-utils.js');
 
-const STATIC_CACHE = 'static-v1';
-const DYNAMIC_CACHE = 'dynamic-v1';
+const STATIC_CACHE = 'static-v1.1';
+const DYNAMIC_CACHE = 'dynamic-v1.1';
 const INMUTABLE_CACHE = 'inmutable-v1';
 
 const APP_SHELL = [
@@ -45,6 +45,9 @@ self.addEventListener('activate', e => {
     const respuesta = caches.keys().then(keys => {
         keys.forEach(key => {
             if (key !== STATIC_CACHE && key.includes('static')) {
+                return caches.delete(key);
+            }
+            if (key !== DYNAMIC_CACHE && key.includes('dynamic')) {
                 return caches.delete(key);
             }
         });
