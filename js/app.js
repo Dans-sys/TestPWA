f3e7ebe29da76d62eecd5c7eb15db0c5f6213f5f
Daniel Solano <daniel.solano@azucareralagrecia.com>
2022-05-30 13:47:14 -0600

    Ruta de SW

--- js/app.js
+++ js/app.js
@@ -1,5 +1,22 @@
+const isLocalhost = Boolean(
+    window.location.hostname === 'localhost' ||
+    // [::1] is the IPv6 localhost address.
+    window.location.hostname === '[::1]' ||
+    // 127.0.0.1/8 is considered localhost for IPv4.
+    window.location.hostname.match(
+        /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
+    )
+);
+
+var url = window.location.href;
+var swLocation = '/twitor/sw.js';
+
 if (navigator.serviceWorker) {
-    navigator.serviceWorker.register('/sw.js');
+    if (isLocalhost) {
+        swLocation = '/sw.js';
+    }
+
+    navigator.serviceWorker.register(swLocation);
 }
 
 
